dfdbe45d747ad83f7f3ab85724d95e0c9d9c27f1
Abraham10000 <hei.abraham.4@gmail.com>
2024-01-30 21:13:26 +0300

    feat : add reaction counter

--- modules/get-last-posts.js
+++ modules/get-last-posts.js
@@ -1,4 +1,5 @@
 const fb = require('fb');
+const processReactions = require('./count-reactions')
 
 async function GetLastPosts(userId, accessToken, limit) {
   try {
@@ -7,7 +8,9 @@ async function GetLastPosts(userId, accessToken, limit) {
     const fields = 'id,created_time,story,message,shares,reactions.type(LIKE).limit(0).summary(1).as(like),reactions.type(LOVE).limit(0).summary(1).as(love),reactions.type(HAHA).limit(0).summary(1).as(haha),reactions.type(WOW).limit(0).summary(1).as(wow),reactions.type(SAD).limit(0).summary(1).as(sad),reactions.type(ANGRY).limit(0).summary(1).as(angry)';
     const params = { fields, limit };
     const response = await fb.api(apiPath, 'GET', params);
-    return response;
+    const reactionsSummary = processReactions(response.data);
+    const value =  {posts : response.data, reactionsSummary};
+    return value;
   } catch (error) {
     throw new Error('Erreur lors de la récupération des publications depuis l\'API Facebook');
   }
