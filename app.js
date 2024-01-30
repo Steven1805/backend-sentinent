ba8a80b28afdd57cb4a5ad262cd13f5c9f6213be
Abraham10000 <hei.abraham.4@gmail.com>
2024-01-31 00:39:30 +0300

    feat : add sentiment analyzer by reactions with get /feed-back

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const analyzeSentiment = require('./modules/analyze-sentiment')
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/feed-back', async (req, res) => {
+  try {
+    const { userId, accessToken, limit } = req.query;
+    if (!userId || !accessToken || !limit) {
+      return res.status(400).json({ error: 'Tous les paramètres nécessaires ne sont pas fournis.' });
+    }
+    const result = await analyzeSentiment(userId, accessToken, limit);
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur interne s\'est produite.' });
+  }
 });
 
 app.listen(port, () => {
