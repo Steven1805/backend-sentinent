b61bbcee6db850192ebd047e27b9007aab442117
Abraham10000 <hei.abraham.4@gmail.com>
2024-01-31 00:21:12 +0300

    feat : add sentiment analyzer by reactions

--- modules/get-posts-selected.js
+++ modules/get-posts-selected.js
@@ -1,18 +1,15 @@
 const fb = require('fb');
-const processReactions = require('./count-reactions')
 
-async function GetLastPosts(userId, accessToken, limit) {
+async function getPostSelected(userId, accessToken, limit) {
   try {
     fb.setAccessToken(accessToken);
     const apiPath = `/${userId}/posts`;
     const fields = 'id,created_time,story,message,shares,reactions.type(LIKE).limit(0).summary(1).as(like),reactions.type(LOVE).limit(0).summary(1).as(love),reactions.type(HAHA).limit(0).summary(1).as(haha),reactions.type(WOW).limit(0).summary(1).as(wow),reactions.type(SAD).limit(0).summary(1).as(sad),reactions.type(ANGRY).limit(0).summary(1).as(angry)';
     const params = { fields, limit };
     const response = await fb.api(apiPath, 'GET', params);
-    const reactionsSummary = processReactions(response.data);
-    const value =  {posts : response.data, reactionsSummary};
-    return value;
+    return response;
   } catch (error) {
-    throw new Error('Erreur lors de la récupération des publications depuis l\'API Facebook');
+    throw new Error(error);
   }
 }
-module.exports = GetLastPosts;
+module.exports = getPostSelected;
