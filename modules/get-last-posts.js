088755eaf5309136bd21febfd4865709acb25816
Abraham10000 <hei.abraham.4@gmail.com>
2024-01-30 20:41:12 +0300

    fix : make limit more dynamic than the last

--- modules/get-last-posts.js
+++ modules/get-last-posts.js
@@ -1,16 +1,15 @@
 const fb = require('fb');
 
-async function GetLastPosts(userId, accessToken) {
+async function GetLastPosts(userId, accessToken, limit) {
   try {
     fb.setAccessToken(accessToken);
     const apiPath = `/${userId}/posts`;
     const fields = 'id,created_time,story,message,shares,reactions.type(LIKE).limit(0).summary(1).as(like),reactions.type(LOVE).limit(0).summary(1).as(love),reactions.type(HAHA).limit(0).summary(1).as(haha),reactions.type(WOW).limit(0).summary(1).as(wow),reactions.type(SAD).limit(0).summary(1).as(sad),reactions.type(ANGRY).limit(0).summary(1).as(angry)';
-    const params = { fields };
+    const params = { fields, limit };
     const response = await fb.api(apiPath, 'GET', params);
     return response;
   } catch (error) {
     throw new Error('Erreur lors de la récupération des publications depuis l\'API Facebook');
   }
 }
-
 module.exports = GetLastPosts;
