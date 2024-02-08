102ebef8a6006d9fcace10bd9efa2d2b31401ec1
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2024-02-08 09:53:13 +0300

    fix: put port as an environment variable

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 4000
 const analyzeSentiment = require('./modules/analyze-sentiment')
 require('dotenv').config()
+const port = process.env.PORT
 
 // Utiliser le middleware cors
 app.use(cors())
